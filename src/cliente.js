a0b503ccd90ed60529791657314258352e93a14b
Luis Felipe <luisfelipe.di@gmail.com>
2024-03-24 22:36:16 -0300

    deploy desafio Impuestos Modulo 4 seccion1

---
+++ src/cliente.js
@@ -0,0 +1,17 @@
+// construimos la clase cliente
+class Cliente{
+    constructor(nombre, impuesto){
+        this._nombre = nombre;
+        this._impuesto = impuesto;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    set nombre(nuevoNombre){
+        this._nombre = nuevoNombre;
+    }
+    calcularImpuesto(){
+        return ((this._impuesto.brutAnual - this._impuesto.deducciones)* 0.21)
+    }
+}
+export default Cliente;
